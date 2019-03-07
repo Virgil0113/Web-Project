4165553c7c14579222d7f48641299913e995c1b0
Virgil0113 <361275898@qq.com>
2019-03-07 23:49:50 +0800

    Update global.js

--- Project 1/Code/scripts/global.js
+++ Project 1/Code/scripts/global.js
@@ -168,3 +168,55 @@ function prepareInternalnav() {
   }
 }
 addLoadEvent(prepareInternalnav);
+
+function showPic(whichpic){
+  if (!document.getElementById("placeholder")) return true;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src",source);
+  if (document.getElementsById("description")) return false;
+  if (whichpic.getAttribute("title")) {
+    var text = whichpic.getAttribute("title");
+  } else {
+    var text = "";
+  }
+  var description = document.getElementById("description");
+  description.firstChild.nodeValue = text;
+  }
+  return false;
+}
+
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var placeholder = document.createElement("img");
+  placeholder.setAttribute("id","placeholder");
+  placeholder.setAttribute("src","images/placeholder.gif");
+  placeholder.setAttribute("alt","iamge gallery");
+  var description = docuement.createElment("p");
+  description.setAttribute("id","description");
+  var desctext = docuement.createTextNode("Choose an image");
+  description.appendChild(desctext);
+  var gallery = document.getElementById("imagegallery");
+  insertAfter(description,gallery);
+  insertAfter(placeholder,description);
+}
+
+function prepareGallery() {
+  if (!docuemnt.getElementByTagName) return false;
+  if (!docuemnt.getElementById) return false;
+  if (!docuemnt.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for (var i=0; i < links.length; i++) {
+     links[i].onclick = function() {
+        showPic(this);
+        return false;
+     }
+  }
+}
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
